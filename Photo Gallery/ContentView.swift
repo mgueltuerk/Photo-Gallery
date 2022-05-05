818e1d426e069b0af506385233f58471ab0ae6f9
Murat Gültürk <m.gueltuerk@gmx.de>
2022-05-05 09:52:53 +0200

    Initial Commit

---
+++ Photo Gallery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photo Gallery
+//
+//  Created by Murat Gültürk on 05.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo Gallery/Photo_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_GalleryApp.swift
+//  Photo Gallery
+//
+//  Created by Murat Gültürk on 05.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Photo_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
